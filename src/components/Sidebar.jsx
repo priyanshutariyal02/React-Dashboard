0fb47577305c9af24dd8e1b98ebc0fc31767ae9d
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2024-05-23 14:28:44 +0530

    complete react dashboard

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import {
+  BsCart3,
+  BsGrid1X2Fill,
+  BsFillArchiveFill,
+  BsFillGrid3X3GapFill,
+  BsPeopleFill,
+  BsListCheck,
+  BsMenuButtonWideFill,
+  BsFillGearFill,
+} from "react-icons/bs";
+
+function Sidebar({ openSidebarToggle, OpenSidebar }) {
+  return (
+    <aside
+      id="sidebar"
+      className={openSidebarToggle ? "sidebar-responsive" : ""}
+    >
+      <div className="sidebar-title">
+        <div className="sidebar-brand flex">
+          <BsCart3 className="icon_header" /> SHOP
+        </div>
+        <span className="icon close_icon" onClick={OpenSidebar}>
+          X
+        </span>
+      </div>
+
+      <ul className="sidebar-list">
+        <li className="sidebar-list-item">
+          <a href="" className="flex gap-3">
+            <BsGrid1X2Fill className="icon" /> Dashboard
+          </a>
+        </li>
+        <li className="sidebar-list-item">
+          <a href="" className="flex gap-3">
+            <BsFillArchiveFill className="icon" /> Products
+          </a>
+        </li>
+        <li className="sidebar-list-item">
+          <a href="" className="flex gap-3">
+            <BsFillGrid3X3GapFill className="icon" /> Categories
+          </a>
+        </li>
+        <li className="sidebar-list-item">
+          <a href="" className="flex gap-3">
+            <BsPeopleFill className="icon" /> Customers
+          </a>
+        </li>
+        <li className="sidebar-list-item">
+          <a href="" className="flex gap-3">
+            <BsListCheck className="icon" /> Inventory
+          </a>
+        </li>
+        <li className="sidebar-list-item">
+          <a href="" className="flex gap-3">
+            <BsMenuButtonWideFill className="icon" /> Reports
+          </a>
+        </li>
+        <li className="sidebar-list-item">
+          <a href="" className="flex gap-3">
+            <BsFillGearFill className="icon" /> Setting
+          </a>
+        </li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Sidebar;
